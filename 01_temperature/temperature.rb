d07326cd3914e865827fbd644a9c27a39665f852
aaron.kai.hubbard <aaron.kai.hubbard@gmail.com>
2019-06-04 17:08:53 -0400

    finished first 3 exercises

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
 #write your code here
+
+def ftoc(temp_in_F)
+	(temp_in_F-32) * (5.0/9.0)
+end
+		
+def ctof(temp_in_Celcius)
+	temp_in_Celcius * (9.0/5.0) + 32
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,35 @@
 #write your code here
+def echo(sound)
+	sound
+end
+
+def shout(sound)
+	sound.upcase
+end
+
+def repeat(word, number=2)
+		("#{word} ")*(number-1) + word		
+end
+
+def start_of_word(word, number)
+	word[0...number]
+end
+
+def first_word(phrase)
+	phrase.split[0]
+end
+
+def titleize(phrase)
+	old_phrase = phrase.split
+	new_phrase = []
+	little_words = ['and','the','over']
+	new_phrase << old_phrase.shift.capitalize 
+	old_phrase.each do |word| 
+		unless (little_words.include? word)
+			new_phrase << word.capitalize
+		else
+			new_phrase << word
+		end
+	end
+	new_phrase.join(' ')
+end
